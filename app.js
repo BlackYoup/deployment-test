0d7c21249d55d30a636f205fde01b025be8778b8
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2020-11-03 12:33:41 +0100

    Canvas?

--- app.js
+++ app.js
@@ -7,8 +7,31 @@ app.get('/', function(req, res){
   res.send(`
   <html>
     <body>
+      <script>
+        var img = new Image();
+        img.crossOrigin = "";
+        img.onload = test;
+        img.src = "http://46.252.181.47:11083/blackyoup/hltv.png";
+
+        function test() {
+
+          var ctx = document.querySelector("canvas").getContext("2d");
+          ctx.drawImage(this, 0, 0);
+
+          // This will fail if no CORS support, otherwise all OK
+          try {
+            ctx.getImageData(0, 0, 10, 10);
+            alert("All OK");
+          }
+          catch(err) {
+            alert("No CORS support...");
+          }
+        }
+      </script>
+
       Hello, second version ! ${process.env.INSTANCE_NUMBER}
-      <img src="http://46.252.181.47:11083/blackyoup/hltv.png" />
+      <!--<img src="http://46.252.181.47:11083/blackyoup/hltv.png" />-->
+      <canvas></canvas>
     </body>
   </html>
 `);
