4978d5318f2dd28888587e53cf0fa943876f1c47
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2024-08-05 11:42:50 +0200

    xx2

--- app.js
+++ app.js
@@ -51,7 +51,9 @@ app.get('/timeout', function(req, res) {
 })
 
 setInterval(() => {
-  console.log(process.env.INSTANCE_NUMBER, new Date().toISOString(), "ROOM");
+  for(let i = 0; i<50; i++) {
+    console.log(process.env.INSTANCE_NUMBER, new Date().toISOString(), "ROOM:", i);
+  }
 }, 1000);
 
 app.listen(process.env.PORT);
